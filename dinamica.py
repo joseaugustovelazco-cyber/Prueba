c85afc09403d964c8af0eb7e2cb4d9bcac47eb30
joseaugustovelazco-cyber <joseaugustovelazco@gmail.com>
2026-06-10 19:11:46 -0300

    Update dinamica.py

--- dinamica.py
+++ dinamica.py
@@ -1,3 +1,16 @@
-1
-1
-1
+def emparejar_datos
+
+
+
+
+
+
+def purificar_archivo(archivo:str)->str:
+  viejo_archivo = open(archivo)
+  nuevo_archivo = open("archivo_limpio.txt", "w")
+  for lineas in viejo_archivo:
+    lineas = emparejar_datos(lineas)
+    nuevo_archivo.write(lineas)
+  viejo_archivo.close()
+  nuevo_archivo.close()
+  
