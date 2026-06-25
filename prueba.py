807f726059d953f3982d69af00e986dd0c960255
joseaugustovelazco-cyber <joseaugustovelazco@gmail.com>
2026-06-25 01:14:06 +0000

    insercion de nuevo archivo

---
+++ prueba.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+
+# Crear figura y ejes
+# fig, ax = plt.subplots()
+
+# # Dibujar datos
+# x = [1, 2, 3, 4]
+# y = [10, 20, 15, 25]
+# ax.plot(x, y, marker="o", color="blue")
+
+# # Personalizar título y etiquetas
+# ax.set_facecolor("lightgray")
+# fig.patch.set_facecolor("black")
+# ax.set_title("Ventas mensuales", fontsize=16, color="darkred")
+# ax.set_xlabel("Meses", fontsize=12, color="green")
+# ax.set_ylabel("Cantidad vendida", fontsize=12, color="purple")
+
+# # Cambiar color de los valores de los ejes
+# ax.tick_params(axis="x", colors="orange")
+# ax.tick_params(axis="y", colors="brown")
+
+# # Mostrar en Streamlit
+# st.pyplot(fig)
+
